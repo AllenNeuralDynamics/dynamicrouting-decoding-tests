f8066edc22163ddeae3a3770d8a319323a43ba62
egmcbride <ethan.mcbride@alleninstitute.org>
2025-09-22 16:31:59 +0000

    Edited app-panel.json, resources.json, decoding_utils.py

--- code/decoding_utils.py
+++ code/decoding_utils.py
@@ -104,7 +104,7 @@ class Params(pydantic_settings.BaseSettings):
     """ filter trials table input to decoder by boolean column or polars expression"""
     label_to_decode: str = 'rewarded_modality'
     """ designate label to decode; corresponds to column in the trials table"""
-    spike_count_intervals: Literal['pre_stim_single_bin', 'binned_stim_and_response', 'pre_stim_single_bin_0.5', 'pre_stim_single_bin_1.5', 'binned_stim_and_response_0.5','binned_stim_0.5','binned_stim_0.1','binned_stim_0.05','binned_stim_only_0.05','binned_stim_only_0.025','binned_stim_only_0.02','binned_stim_only_0.01'] = 'pre_stim_single_bin'
+    spike_count_intervals: Literal['pre_stim_single_bin', 'binned_stim_and_response', 'pre_stim_single_bin_0.5', 'pre_stim_single_bin_1.5', 'binned_stim_and_response_0.5','binned_stim_0.5','binned_stim_0.1','binned_stim_0.05','binned_stim_only_0.05','binned_stim_only_0.025','binned_stim_only_0.02','binned_stim_only_0.01','binned_stim_onset_only_0.01'] = 'pre_stim_single_bin'
     baseline_subtraction: bool = False
     """whether to subtract the average baseline context modulation from each unit/trial"""
     n_blocks_expected: int = 6
@@ -736,7 +736,7 @@ def wrap_decoder_helper(
                     result['time_aligned_to'] = interval_config.event_column_name
                     result['bin_size'] = interval_config.bin_size
                     if params.use_cumulative_spike_counts:
-                        result['bin_center'] = (start_original + stop) / 2
+                        result['bin_center'] = stop
                     elif params.sliding_window_size is not None:
                         result['bin_center'] = (start_original + stop_original) / 2
                         #result['bin_center'] = stop
@@ -746,7 +746,7 @@ def wrap_decoder_helper(
                     result['repeat_idx'] = repeat_idx
                     
                     if shift in (0, None):  
-                        if params.label_to_decode=="is_response" or params.label_to_decode=="is_target":
+                        if params.label_to_decode in ["is_response", "is_target", "is_rewarded"]:
                             result['predict_proba'] = _result['predict_proba'][:, np.where(_result['label_names'] == True)[0][0]].tolist()
                             result['predict_proba_all_trials'] = _result['predict_proba_all_trials'][:, np.where(_result['label_names'] == True)[0][0]].tolist()
                         elif params.label_to_decode=="stim_name":
